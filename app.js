2a2c40ecb0c588ac598edcaf18eafe001d7b5df9
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2017-11-21 21:26:41 +0100

    .env fitxategia. REST OK

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const Sequelize = require('sequelize'),
+    epilogue = require('epilogue'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    http=require('http');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+require('dotenv').config()
 
-var app = express();
+// Define your models with Sequelize
+// This is equivalent to defining Doctrine entities
+let database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+database.authenticate().then(() => {
+    console.log('Connection has been established successfully.'); })
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
 });
 
-module.exports = app;
+let Satisfaction = database.define('Satisfaction', {
+    kokapena: Sequelize.STRING,
+    emaitza: Sequelize.INTEGER
+
+});
+
+// Initialize server
+let server, app;
+if (process.env.USE_RESTIFY) {
+    const restify = require('restify');
+
+    app = server = restify.createServer()
+    app.use(restify.queryParser());
+    app.use(restify.bodyParser());
+} else {
+    const express = require('express'),
+        bodyParser = require('body-parser');
+
+    app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    server = http.createServer(app);
+}
+
+
+// Plug Epilogue into Express
+epilogue.initialize({
+    app: app,
+    sequelize: database
+});
+
+// Configure a REST resource endpoint with Epilogue
+let satisfactionResource = epilogue.resource({
+    model: Satisfaction,
+    endpoints: ['/satisfaction', '/satisfaction/:id']
+});
+
+database
+    .sync({ force: false })
+    .then(function() {
+        app.listen(function() {
+            // var host = app.address().address,
+            //     port = app.address().port;
+
+            console.log('Zerbitzaria entzuten');
+        });
+    });
