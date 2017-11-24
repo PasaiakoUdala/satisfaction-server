0fa4f710ddf93a45a659ab895d9c52f042bfb849
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2017-11-24 14:20:40 +0100

    CORS headers

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ if (process.env.USE_RESTIFY) {
   app = server = restify.createServer();
   app.use(restify.queryParser());
   app.use(restify.bodyParser());
+
 } else {
   const express = require('express'),
     bodyParser = require('body-parser');
@@ -50,6 +51,25 @@ if (process.env.USE_RESTIFY) {
   app.use(bodyParser.urlencoded({extended: false}));
   server = http.createServer(app);
 }
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 
 // Plug Epilogue into Express
