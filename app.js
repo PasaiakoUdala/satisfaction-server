3168a8966576fb1f258b490fbfd207b7d9e53625
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2017-11-24 14:14:07 +0100

    prest

--- app.js
+++ app.js
@@ -1,72 +1,74 @@
 const Sequelize = require('sequelize'),
-    epilogue = require('epilogue'),
-    http=require('http');
+  epilogue = require('epilogue'),
+  http = require('http');
 
 require('dotenv').config()
 
 // Define your models with Sequelize
 // This is equivalent to defining Doctrine entities
 let database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 database.authenticate().then(() => {
-    console.log('Connection has been established successfully.'); })
-.catch(err => {
+  console.log('Connection has been established successfully.');
+})
+  .catch(err => {
     console.error('Unable to connect to the database:', err);
-});
+  });
 
 let Satisfaction = database.define('Satisfaction', {
-    kokapena: Sequelize.STRING,
-    saila: Sequelize.STRING,
-    galdera: Sequelize.STRING,
-    emaitza: Sequelize.INTEGER
+  kokapena: Sequelize.STRING,
+  saila: Sequelize.STRING,
+  galderaeus: Sequelize.STRING,
+  galderaes: Sequelize.STRING,
+  emaitza: Sequelize.INTEGER
 });
 
 // Initialize server
 let server, app;
 if (process.env.USE_RESTIFY) {
-    const restify = require('restify');
+  const restify = require('restify');
 
-    app = server = restify.createServer()
-    app.use(restify.queryParser());
-    app.use(restify.bodyParser());
+  app = server = restify.createServer();
+  app.use(restify.queryParser());
+  app.use(restify.bodyParser());
 } else {
-    const express = require('express'),
-        bodyParser = require('body-parser');
+  const express = require('express'),
+    bodyParser = require('body-parser');
 
-    app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    server = http.createServer(app);
+  app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
+  server = http.createServer(app);
 }
 
 
 // Plug Epilogue into Express
 epilogue.initialize({
-    app: app,
-    sequelize: database
+  app: app,
+  sequelize: database
 });
 
 // Configure a REST resource endpoint with Epilogue
 let satisfactionResource = epilogue.resource({
-    model: Satisfaction,
-    endpoints: ['/satisfaction', '/satisfaction/:id']
+  model: Satisfaction,
+  endpoints: ['/satisfaction', '/satisfaction/:id']
 });
 
 database
-    .sync({ force: false })
-    .then(function() {
-        app.listen(3000,function() {
-          var host = app.address;
-            console.log('Zerbitzaria entzuten http://localhost:3000');
-        });
+  .sync({force: false})
+  .then(function () {
+    app.listen(3000, function () {
+      var host = app.address;
+      console.log('Zerbitzaria entzuten http://localhost:3000');
     });
+  });
